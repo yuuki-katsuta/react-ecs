77ceb074971f42f9573cfbd5aa31642714e22cd0
tpryuki <reptpr0314@gmail.com>
2026-01-21 18:28:27 +0900

    🚧

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,15 @@ ENV_CONFIG_FILE="/usr/share/nginx/html/env-config.js"
 
 # 環境変数を読み取ってenv-config.jsを生成
 cat <<EOF > $ENV_CONFIG_FILE
-window.env = {
+const config = Object.freeze({
   SAMPLE_KEY: '${SAMPLE_KEY:-}',
-};
+})
+
+Object.defineProperty(window, "env", {
+  value:config,
+  writable: false,
+  configurable: false
+})
 EOF
 
 echo "環境変数を注入しました:"
